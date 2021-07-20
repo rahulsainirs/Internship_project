91fa92430edb0045e17066a0b82650924733c2fe
rahul saini <rahulsainirs8800@gmail.com>
2021-07-20 21:25:25 +0530

    first commit

---
+++ app/src/main/java/com/rahulcodecamp/menstruationcare/HomeActivity.java
@@ -0,0 +1,195 @@
+package com.rahulcodecamp.menstruationcare;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mikhaellopez.circularimageview.CircularImageView;
+import com.squareup.picasso.Picasso;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle toggle;
+
+
+    FirebaseAuth auth;
+    FirebaseDatabase firebaseDatabase;
+
+    ProgressDialog progressDialog;
+    public ArrayList<String> profileItems;
+    LottieAnimationView headerBackground;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        auth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        toolbar = findViewById(R.id.toolBar);
+        navigationView = findViewById(R.id.navigationView);
+
+        headerBackground = findViewById(R.id.headerBackground);  // animated header background
+
+
+        setSupportActionBar(toolbar);   // below three lines are to create toggle icon with function
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (item.getItemId()){
+                    case (R.id.logoutBtn):
+                        Dialog dialog = new Dialog(HomeActivity.this,R.style.Dialog);
+                        dialog.setContentView(R.layout.logout_dialog_layout);
+                        Button yesButton, noButton;
+                        yesButton = dialog.findViewById(R.id.yesButton);
+                        noButton = dialog.findViewById(R.id.noButton);
+
+                        yesButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+//                                FirebaseAuth.getInstance().signOut();
+                                auth.signOut();
+                                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                            }
+                        });
+                        noButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialog.dismiss();
+                            }
+                        });
+                        dialog.show();
+                        break;
+                    case (R.id.chatsBtn):
+                        Toast.makeText(HomeActivity.this, "chats clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    }
+                    return true;
+                    // we will add further cases according to data and uses
+            }
+        });
+
+//        View view = navigationView.getHeaderView(0);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("please wait...");
+        progressDialog.setCancelable(false);
+
+        progressDialog.show();
+
+
+        if (currentUser == null){
+            progressDialog.dismiss();
+            startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+        }
+
+        else if (!currentUser.isEmailVerified()){
+            progressDialog.dismiss();
+            Toast.makeText(this, "your email is not verified", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            finish();
+
+        }
+        else if (currentUser.isEmailVerified()){
+            progressDialog.dismiss();
+
+            Toast.makeText(this, "Email is verified", Toast.LENGTH_SHORT).show();
+
+            DatabaseReference databaseReference = firebaseDatabase.getReference().child("user");
+
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                    for (DataSnapshot dataSnapshot: snapshot.getChildren() ){
+                        try {
+                            if (dataSnapshot.getKey().equals(currentUser.getUid())){
+                                Toast.makeText(HomeActivity.this, "uid matched", Toast.LENGTH_SHORT).show();
+                                profileItems = new ArrayList<>();
+                                profileItems.add(dataSnapshot.child("name").getValue().toString());
+                                profileItems.add(dataSnapshot.child("email").getValue().toString());
+
+                                profileItems.add(dataSnapshot.child("imageUri").getValue().toString());
+
+                                TextView nameTextView = findViewById(R.id.nameTextView);
+                                TextView emailTextView = findViewById(R.id.emailTextView);
+                                CircularImageView profileImageView = findViewById(R.id.profileImageView);
+
+                                nameTextView.setText(profileItems.get(0));
+                                emailTextView.setText(profileItems.get(1));
+                                Picasso.get().load(profileItems.get(2)).into(profileImageView);
+                            }
+                        }
+                        catch (Exception e){
+                            Toast.makeText(HomeActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+                }
+            });
+        }
+        else {
+            progressDialog.dismiss();
+            Toast.makeText(this, "Verify your email", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+        }
+    }
+
+
+    @Override    // to close the drawer when we touch/press back button
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            onDestroy();
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/rahulcodecamp/menstruationcare/Users.java
@@ -0,0 +1,72 @@
+package com.rahulcodecamp.menstruationcare;
+
+public class Users {  // we need this model class to crate database having these below mentioned field
+
+    String uid;
+    String name;
+    String phoneNo;
+    String address;
+    String email;
+    String imageUri;
+
+    public Users() {
+
+    }
+
+    public Users(String uid, String name, String phoneNo, String address, String email, String imageUri) {
+        this.uid = uid;
+        this.name = name;
+        this.address = address;
+        this.phoneNo = phoneNo;
+        this.email = email;
+        this.imageUri = imageUri;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+}
